02a537c632822f10d335221d5ace7bc48c916b98
Luís Felipe Nogoseke <luis.nogoseke@pucpr.edu.br>
2016-09-14 09:29:33 -0300

    Funcionando (+-)

--- clientfeira.go
+++ clientfeira.go
@@ -31,12 +31,15 @@ func main () {
 	client, err := rpc.Dial("tcp", service)
 	defer client.Close()
 	checkError("Dial: ", err)
-	fmt.Println("* - multiplicação")
-	fmt.Println("/ - divisão")
+	fmt.Println("1 - AddFruit")
+	fmt.Println("2 - GetPrice")
+	fmt.Println("3 - GetPriceKg")
+	fmt.Println("4 - RemoveFruit")
+	fmt.Println("5 - UpdatePrice")
 	var op byte
 	fmt.Scanf("%c\n", &op)
 	switch op {
-		case '*':
+	case '1':
 			args := readArgs()
 			var reply int
 			mulCall := client.Go("Fruit.AddFruit",	args, &reply, nil)
@@ -44,12 +47,32 @@ func main () {
 			checkError("Multiply: ", replyMul.Error)
 			// fmt.Printf("%d * %d = %d\n", args.A, args.B, reply)
 			os.Exit(0)
-		case '/':
+		case '2':
 			args := readArgs()
-			var reply int
+			var reply float64
+			divCall := client.Go("Fruit.GetPrice", args, &reply, nil)
+			replyDiv := <- divCall.Done
+			checkError("Divide: ", replyDiv.Error)
+			fmt.Printf("%.2f\n", reply)
+		case '3':
+			args := readArgs()
+			var reply float64
 			divCall := client.Go("Fruit.GetPriceKg", args, &reply, nil)
 			replyDiv := <- divCall.Done
 			checkError("Divide: ", replyDiv.Error)
+			fmt.Printf("%.2f\n", reply)
+		case '4':
+			args := readArgs()
+			divCall := client.Go("Fruit.RemoveFruit", args, nil, nil)
+			replyDiv := <- divCall.Done
+			checkError("Divide: ", replyDiv.Error)
+		case '5':
+			args := readArgs()
+			var reply int
+			divCall := client.Go("Fruit.UpdatePrice", args, &reply, nil)
+			replyDiv := <- divCall.Done
+			checkError("Divide: ", replyDiv.Error)
+				fmt.Printf("%.2f\n", reply)
 			// fmt.Printf("%d / %d = (%d,%d)\n", args.A, args.B, reply.Q, reply.R)
 			os.Exit(0)
 	}
--- servidordafeira.go
+++ servidordafeira.go
@@ -5,10 +5,13 @@ import (
 	"net"
 	"os"
 	"encoding/csv"
+  "io"
+  "strconv"
 )
 
 var reader *csv.Reader
 var writer *csv.Writer
+var database map[string]float64
 
 type Args struct {
   Name string;
@@ -25,51 +28,89 @@ func checkError(st string, err error) {
 
 type Fruit int
 
+func printMap(m map[string]float64) {
+    for key, value := range m {
+    	fmt.Println(key, "=", value)
+    }
+}
+
+
+func UpdateCsv (m map[string]float64) {
+  os.Remove("feira.csv")
+  file, err := os.Create("feira.csv")
+  checkError("Cannot create file", err)
+  writer = csv.NewWriter(file)
+  for name, price := range m {
+    s := []string{name, fmt.Sprintf("%.2f", price)}
+  	err := writer.Write(s)
+  	writer.Flush()
+    checkError("Cannot write to file", err)
+  }
+}
+
 func (t *Fruit) AddFruit (args *Args, reply *int) error {
 	fmt.Println("AddFruit: ", args.Name, args.Price)
 	s := []string{args.Name, fmt.Sprintf("%.2f", args.Price)}
 	err := writer.Write(s)
 	writer.Flush()
-    checkError("Cannot write to file", err)
+  checkError("Cannot write to file", err)
+  database[args.Name] = args.Price
 	return nil
 }
 
 func (t *Fruit) UpdatePrice (args *Args, reply *int) error {
 	fmt.Println("UpdatePrice", args.Name, args.Price)
+  database[args.Name] = args.Price
+  printMap(database)
+  UpdateCsv(database)
 	return nil
 }
 
 func (t *Fruit) RemoveFruit (args *Args, reply *int) error {
-	fmt.Println("", args.Name, args.Price)
+	fmt.Println("Remove", args.Name, args.Price)
+  printMap(database)
+  delete(database, args.Name)
 	return nil
 }
 
-func (t *Fruit) GetPriceKg (args *Args, reply *int) error {
+func (t *Fruit) GetPriceKg (args *Args, reply *float64) error {
 	fmt.Println("GetPriceKg", args.Name, args.Price)
+  *reply = database[args.Name]
 	return nil
 }
 
-func (t *Fruit) GetPrice (args *Args, reply *int) error {
+func (t *Fruit) GetPrice (args *Args, reply *float64) error {
 	fmt.Println("GetPrice", args.Name, args.Price)
+  *reply = database[args.Name] * args.Price
 	return nil
 }
 
 
 
 func main () {
-
+  database = map[string]float64{}
 	file, err := os.Open("feira.csv")
 	if err != nil {
 		// se nao existir cria
 		file, err = os.Create("feira.csv")
 		checkError("Cannot create file", err)
-	}
+	}  else {
+    reader = csv.NewReader(file)
+    reader.Comma = ';'
+    for {
+      record, err := reader.Read()
+      // Stop at EOF.
+      if err == io.EOF {
+        break
+      }
+        database[record[0]], _ = strconv.ParseFloat(record[1],64)
+    }
+  }
+
 	defer file.Close()
-	reader = csv.NewReader(file)
 	writer = csv.NewWriter(file)
-	reader.Comma = ';'
 	writer.Comma = ';'
-	
+
 	fruit := new(Fruit)
 	rpc.Register(fruit)
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "localhost:1234")
@@ -82,4 +123,3 @@ func main () {
 		rpc.ServeConn(conn)
 	}
 }
-
